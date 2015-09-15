3408e2992b9e1c09ee7e48dd87a0588b967f795e
Purinda Gunasekara <purinda@gmail.com>
2015-09-15 11:05:10 +1000

    Improved the redis db scan lua function

--- tests/test.lua
+++ tests/test.lua
@@ -1,7 +1,7 @@
 -- Following lua script can be used for retrieving hierarchically constructed keys from Redis.
 -- redis-cli "keys" "*" | grep -o -E "^[[:alpha:]]+:^C| sort | uniq
 
-local pattern = 'srt:article-article:249:articles:*';
+local pattern = '*';
 
 -- Count the Nth tree-level within the data structure
 local _, count = string.gsub(pattern, ':', ':')
@@ -9,18 +9,12 @@ local key_pattern = '^';
 
 -- Build string matching pattern for branch nodes as per provided keys pattern
 if (count == 0) then
-    key_pattern = key_pattern .. '(%w+):.*'
+    key_pattern = key_pattern .. '([%a%d\-]+):.*'
 elseif (count == 1) then
-    key_pattern = key_pattern .. '.*:(.+):.*$'
+    key_pattern = key_pattern .. '[%a%d\-]+:([%a%d\-]+).*$'
 else
     key_pattern = key_pattern .. string.rep('[%a%d\-]+:', count)
-    key_pattern = key_pattern .. '([%a%d\-]+).*$';
---    key_pattern = key_pattern .. string.gsub(pattern, ':*', ']:(.+)') .. '$'
-
-    -- doc:article:* -> ^.+:.+:(.+)[:.*]?$
-    -- srt:article-article:* -> ^[%a%d\-]+:[%a%d\-]+:([%a%d\-]+).*$
-
---    key_pattern = '^[%a%d\-]+:([%a%d\-]+).*$'
+    key_pattern = key_pattern .. '([%a%d\-]+):.*$';
 end
 
 local matches = redis.call('KEYS', pattern)
